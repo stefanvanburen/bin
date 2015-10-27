eff387a9b99cfa955aa80e99b2515c6d6c4ed508
Stefan VanBuren <vanburenstefan@gmail.com>
2015-10-26 22:27:29 -0400

    Initial commit of bin dir

---
+++ dual_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output DVI-1 --mode 1920x1080 --output DVI-0 --mode 1920x1080 --right-of DVI-1
---
+++ swap_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --auto --output HDMI-0 --auto --right-of DVI-I-1
